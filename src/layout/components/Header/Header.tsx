7482d1118bff62603694bd4d2843b399c2438f65
Petr Lobachev <lobachevpetr@gmail.com>
2023-01-30 20:27:31 +0300

    improve/upd Header & Footer

--- src/layout/components/Header/Header.tsx
+++ src/layout/components/Header/Header.tsx
@@ -5,10 +5,14 @@ import Logo from "../../../images/logo.png";
 export const Header: React.FC = () => {
   return (
     <header className={Styles.header}>
-      <img className={Styles.header__logo} src={Logo} alt="logo" />
-      <a className={Styles.header__phone} href="#" target="#">
-        +7 (495) 495-49-54
-      </a>
+      <div className={Styles.header__container}>
+        <img className={Styles.header__logo} src={Logo} alt="logo" />
+        <nav>
+          <a className={Styles.header__phone} href="#" target="#">
+            +7 (495) 495-49-54
+          </a>
+        </nav>
+      </div>
     </header>
   );
 };
